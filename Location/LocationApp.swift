1880d81f3d0d26643aeaada2bcea7c7dd099f2e8
Tram Bui <pbui17@cmc.edu>
2021-01-16 18:25:04 -0600

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Tram Bui on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
