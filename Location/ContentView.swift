493a48700b658335212b688f7b47d70925bf80c7
Tram Bui <pbui17@cmc.edu>
2021-01-22 18:50:26 -0600

    initial commit

--- Location/ContentView.swift
+++ Location/ContentView.swift
@@ -6,16 +6,84 @@
 //
 
 import SwiftUI
+import CoreLocation
+import UIKit
+
 
 struct ContentView: View {
+    
+    class ViewController: UIViewController, CLLocationManagerDelegate {
+        let manager = CLLocationManager()
+
+        override func viewDidLoad() {
+            manager.delegate = self
+            manager.requestLocation()
+        }
+
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.first {
+                print("Found user's location: \(location)")
+            }
+        }
+
+        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("Failed to find user's location: \(error.localizedDescription)")
+        }
+    }
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double {
+        // calculate the total per person
+        var peopleCount = Double(numberOfPeople + 2)
+        var tipSeletion = Double(tipPercentages[tipPercentage])
+        
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        var tipValue = orderAmount / 100 * tipSeletion
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("HOw much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Each person pays:")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+        }
+        .navigationBarTitle("WeSplit")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
